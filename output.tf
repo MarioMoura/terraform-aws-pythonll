724772c2f6768d6ce60c0a04d176eaf290feaf00
MarioMoura <mm@mariomoura.com>
2025-09-14 19:49:56 -0300

    feat: remove layersize output from terraform module

--- output.tf
+++ output.tf
@@ -1,9 +1,6 @@
 output "reqs" {
   value = local.requirements_str
 }
-output "layersize" {
-  value = data.external.layer_info.result.layersize
-}
 output "arn" {
   value = aws_lambda_layer_version.lambda_layer.arn
 }
